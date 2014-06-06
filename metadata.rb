112c5328be64d9a1574cb8c0ebe2184fdff31254
Charles Gardner <charles.gardner@aditazz.com>
2014-06-06 16:42:07 -0700

    Merged

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'SoftEther'
+maintainer       'Charles Gardner'
+maintainer_email 'cgardner2020@gmail.com'
+license          'GPLv2'
+description      'Installs/Configures SoftEther VPN Servers and Clients'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+
