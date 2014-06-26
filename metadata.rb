da7bb4e5fd071165adb494a4d103816b93d2b06f
Charles Gardner <charles.gardner@aditazz.com>
2014-06-26 12:15:21 -0700

    Minor revision tick.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email    'cgardner2020@gmail.com'
 license             'GPLv3'
 description         'Installs/Configures SoftEther VPN Servers and Clients'
 long_description    IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version             '0.1.2'
+version             '0.1.3'
 
 depends             'apt'
 depends             'build-essential'
